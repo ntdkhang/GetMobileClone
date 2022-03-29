65265663d3eacfa8be394af514c8833121d66ec1
Khang <khang.nguyen.trd@gmail.com>
2022-03-28 23:23:42 -0500

    Initial Commit

---
+++ GetMobileClone/GetMobileCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMobileCloneApp.swift
+//  GetMobileClone
+//
+//  Created by Nguyen Tran Duy Khang on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct GetMobileCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
